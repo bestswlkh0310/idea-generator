49922d0f51edb37530ce7c450d2156bb3de4a307
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-02-25 20:17:44 +0900

    feat : create shadow component

---
+++ src/component/theme/Shadow.js
@@ -0,0 +1,13 @@
+import { css } from "styled-components";
+
+export const shadow1 = css`
+    box-shadow: 0 4px 10px 0 var(--gray-200);
+`;
+
+export const shadow2 = css`
+    box-shadow: 0 8px 10px 0 var(--gray-300);
+`;
+
+export const shadow3 = css`
+    box-shadow: 0 10px 20px 0 var(--gray-400);
+`;
